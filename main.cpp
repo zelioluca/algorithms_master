36ab181513528ea843ed31c61382ee571bd1add7
luca <luca.zelioli@abo.fi>
2021-03-17 10:52:09 +0200

    added the main fdx

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
+#include "Search.h"
 using namespace std;
 
 int main(int argc, char**argv)
 {
+    //let s prepare the arrays that we need to run our algos
+    srand((unsigned) time(NULL));
+    const int N = 1000;
+
+    int *RandomInput = (int*)malloc(N * sizeof(int));
+    int SequentialInput[N];
+
+    for(int i = 0; i < N; i++)
+    {
+        *(RandomInput + i) = rand() % N;
+        SequentialInput[i] = i;
+    }
+
+    int numberToSearch = 38;
+
+    int rndSearchBinary = BinarySearch(RandomInput, numberToSearch, 0, N-1);
+    cout << "Rand array => the number  " << numberToSearch << " is at position " << rndSearchBinary << endl;
+    int rndSearchSeq = SequentialSearch(RandomInput, numberToSearch, 0, N-1);
+    cout << "Seq array => the number  " << numberToSearch << " is at position " << rndSearchSeq << endl;
+
+    int seqSearchBinary = BinarySearch(SequentialInput, numberToSearch, 0, N-1);
+    cout << "Rand array => the number  " << numberToSearch << " is at position " << seqSearchBinary << endl;
+    int seqSearchSeq = SequentialSearch(SequentialInput, numberToSearch, 0, N-1);
+    cout << "Seq array => the number  " << numberToSearch << " is at position " << seqSearchSeq << endl;
+
     return 0;
 }
 
