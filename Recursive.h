bc2f5f499e64dc15d00509283e6ccca06c49bcb2
luca <luca.zelioli@abo.fi>
2021-03-27 09:19:58 +0200

    add another recursive example

--- Recursive.h
+++ Recursive.h
@@ -17,4 +17,37 @@ int Factorial(int number)
     }
 }
 
+void BinaryRecursive(int n, char*input)
+{
+    if(n < 1)
+    {
+        std::cout << *input << std::endl;
+    }
+    else
+    {
+        *(input - 1) = 0;
+        BinaryRecursive(n-1, input);
+        *(input -1) = 1;
+        BinaryRecursive(n-1, input);
+    }
+}
+
+void StringGenerator(int n, int k, char*input)
+{
+    if(n < 1)
+    {
+        std::cout << input << std::endl;
+    }
+    else
+    {
+        for(int j = 0; j < k; j++)
+        {
+            *(input -1) = j;
+            StringGenerator(n -1, k, input);
+        }
+    }
+}
+
+
+
 #endif //ALGORITHMS_RECURSIVE_H
